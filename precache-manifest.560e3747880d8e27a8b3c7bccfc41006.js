62b8aa3c499f8d5a1379c793b7f84e0bd7d73006
iggy@ivstudio.com <iggy@honeybook.com>
2019-05-09 09:24:15 -0400

    Updates

---
+++ precache-manifest.560e3747880d8e27a8b3c7bccfc41006.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90a00004c2ccb9b02a07199fda0c0ee",
+    "url": "/tdd-click-counter/index.html"
+  },
+  {
+    "revision": "2425a81549bb0eaeb2a8",
+    "url": "/tdd-click-counter/static/css/main.420bb8a8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/tdd-click-counter/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2425a81549bb0eaeb2a8",
+    "url": "/tdd-click-counter/static/js/main.39e93572.chunk.js"
+  },
+  {
+    "revision": "12086216f0e7ea8fe142",
+    "url": "/tdd-click-counter/static/js/runtime~main.f08fe342.js"
+  }
+]);
